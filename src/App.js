f9c0e43dd38ed1070e3ded2eeeb45242f9acd3d1
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2020-11-10 23:50:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,21 +7,14 @@ import ContactSkeletonLoading from './ContactSkeletonLoading';
 
 const App = () => {
   const [contacts, setContacts] = React.useState([
-    { "id": 1, "photo": Avatar, "name": "Tamma Everill", "phone": "+351 888 411 5474" },
-    { "id": 2, "photo": Avatar, "name": "Alejandrina Alexis", "phone": "+62 188 649 7200" },
-    { "id": 3, "photo": Avatar, "name": "Hakim Bruntjen", "phone": "+86 241 773 8545" },
-    { "id": 4, "photo": Avatar, "name": "Prudi Dagwell", "phone": "+62 606 216 1097" },
-    { "id": 5, "photo": Avatar, "name": "Prent Frizell", "phone": "+86 808 891 5427" },
-    { "id": 6, "photo": Avatar, "name": "Curtis Enterlein", "phone": "+64 836 110 1773" },
-    { "id": 7, "photo": Avatar, "name": "Tamma Everill", "phone": "+351 888 411 5474" },
-    { "id": 8, "photo": Avatar, "name": "Alejandrina Alexis", "phone": "+62 188 649 7200" },
-    { "id": 9, "photo": Avatar, "name": "Hakim Bruntjen", "phone": "+86 241 773 8545" },
-    { "id": 10, "photo": Avatar, "name": "Prudi Dagwell", "phone": "+62 606 216 1097" },
-    { "id": 11, "photo": Avatar, "name": "Prent Frizell", "phone": "+86 808 891 5427" },
-    { "id": 12, "photo": Avatar, "name": "Curtis Enterlein", "phone": "+64 836 110 1773" },
-    { "id": 13, "photo": Avatar, "name": "Margret Brissard", "phone": "+372 242 306 0100" },
-    { "id": 14, "photo": Avatar, "name": "Margret Brissard", "phone": "+372 242 306 0100" },
-  ]);
+    { "id": 1, "image":{ src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},"photo": Avatar, "name": "Tamma Everill", "phone": "+351 888 411 5474" },
+    { "id": 2, "image":{ src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},"photo": Avatar, "name": "Alejandrina Alexis", "phone": "+62 188 649 7200" },
+    { "id": 3, "image":{ src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},"photo": Avatar, "name": "Hakim Bruntjen", "phone": "+86 241 773 8545" },
+    { "id": 4, "image":{ src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},"photo": Avatar, "name": "Prudi Dagwell", "phone": "+62 606 216 1097" },
+    { "id": 5, "image":{ src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},"photo": Avatar, "name": "Prent Frizell", "phone": "+86 808 891 5427" },
+    { "id": 6, "image":{ src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},"photo": Avatar, "name": "Curtis Enterlein", "phone": "+64 836 110 1773" },
+    { "id": 7, "image":{ src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},"photo": Avatar, "name": "Tamma Everill", "phone": "+351 888 411 5474" },
+      ]);
 
   const fetchMoreData = () => {
     const newData = [...contacts];
@@ -43,7 +36,7 @@ const App = () => {
       >
         {contacts.map(({ id, photo, name, phone }) => (
           <div className="contact-container" key={id}>
-            <img src={photo} alt={name} className="avatar" />
+            <img src={image} alt={name} className="avatar" />
             <div className="text-container">
               <h5 className="name">{name}</h5>
               <p className="phone">{phone}</p>
